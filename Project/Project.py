abc3d5ef560e0ddc509e537e6f76acf8b39c411b
Francis Surroca <fsurroca@microsoft.com>
2022-07-17 11:28:17 -0400

    Investigation and visualizations

--- Project/Project.py
+++ Project/Project.py
@@ -13,22 +13,23 @@
 """
 
 import pandas as pd
+import numpy as np
 import os
 from Enums import *
 from Constants import *
 from Globals import *
+import matplotlib.pyplot as plt
 
 """
 ##################################################
 #
-# Questions 3: bring in the dataset
+# Step 3: bring in the dataset
 # 
 ##################################################
 """
 folder = os.getcwd()
-os.chdir(folder + "\\Project\\Data")
-print(os.getcwd())
-print(os.listdir())
+git_path = 'C:\\Users\\fsurroca\\Documents\\GitHub\\ECO5445SU22'
+os.chdir(git_path + "\\Project\\Data")
 df = pd.read_csv("hmda_sw.csv", delimiter=',')
 
 df.rename(columns=COLUMN_MAPPING, inplace=True)
@@ -40,17 +41,63 @@ df_model_data = df.filter(COLUMN_NAMES)
 """
 ##################################################
 #
-# Questions 4: generate summary statistics
+# Step 4: generate summary statistics, histograms
 # 
 ##################################################
 """
-sum_stats = df_model_data.describe(include='all')
+# clean the data before interpretation
+
+# set all MISSING_OBSERVATION values to 0 to make data easier to work with
+df_model_data.loc[df_model_data[COLUMN_NAMES[Columns.YEARS_OF_ED]] == MISSING_OBSERVATION, COLUMN_NAMES[Columns.YEARS_OF_ED]] = 0
+
+# set type of action to either just approved/not approved
+df_model_data.loc[df_model_data[COLUMN_NAMES[Columns.TYPE_OF_ACTION]] > ActionTypes.APP_APPROVED_NOT_ACCEPTED, COLUMN_NAMES[Columns.TYPE_OF_ACTION]] = 0
+df_model_data.loc[df_model_data[COLUMN_NAMES[Columns.TYPE_OF_ACTION]] <= ActionTypes.APP_APPROVED_NOT_ACCEPTED, COLUMN_NAMES[Columns.TYPE_OF_ACTION]] = 1
+
+# convert marital status to float data type
+df_model_data.loc[df_model_data[COLUMN_NAMES[Columns.MARITAL_STATUS]] == 'M', COLUMN_NAMES[Columns.MARITAL_STATUS]] = 1
+df_model_data.loc[df_model_data[COLUMN_NAMES[Columns.MARITAL_STATUS]] == 'U', COLUMN_NAMES[Columns.MARITAL_STATUS]] = 0
+df_model_data.loc[df_model_data[COLUMN_NAMES[Columns.MARITAL_STATUS]] == 'S', COLUMN_NAMES[Columns.MARITAL_STATUS]] = 2
+df_model_data.loc[df_model_data[COLUMN_NAMES[Columns.MARITAL_STATUS]] == 'NA', COLUMN_NAMES[Columns.MARITAL_STATUS]] = -1
+df_model_data = df_model_data.astype({COLUMN_NAMES[Columns.MARITAL_STATUS]:float})
+
+# get rid of huge outliers in dept to income
+
+df_model_data.loc[df_model_data[COLUMN_NAMES[Columns.DEPT_TO_INC]] > 100.0, COLUMN_NAMES[Columns.DEPT_TO_INC]] = df_model_data[COLUMN_NAMES[Columns.DEPT_TO_INC]].mean()
+
+# get and display summary statistics
+sum_stats = df_model_data.describe()
 print(f'{sum_stats}\n')
+
+# generate default histograms
+df_model_data.hist()
+
+# investigate data grouped by race
+grouped_by_race = df_model_data.groupby(COLUMN_NAMES[Columns.RACE])
+print(grouped_by_race.describe())
+grouped_by_race.hist()
+
+# investigate correlations between race and all other variables
+for col in COLUMN_NAMES:
+    print(f"Correlation between Race and {col}\n {np.corrcoef(df_model_data[COLUMN_NAMES[Columns.RACE]], df_model_data[col])}")
+
+# visualize some relationships plotted together on same graph
+black = df_model_data[COLUMN_NAMES[Columns.RACE]] == Races.BLACK
+white = df_model_data[COLUMN_NAMES[Columns.RACE]] == Races.WHITE
+for idx, col in enumerate(COLUMN_NAMES):
+    plt.figure(idx+10)
+    plt.style.use('ggplot')
+    plt.title(col)
+    plt.hist(df_model_data[col][white], edgecolor='black',color='blue',label=RACES_MAPPING[Races.WHITE])
+    plt.hist(df_model_data[col][black], edgecolor='black',color='brown',label=RACES_MAPPING[Races.BLACK])
+    plt.legend(loc='upper right')
+    plt.tight_layout()
+    plt.show()
 
 """
 ##################################################
 #
-# Questions 5: baseline probability of an individual
+# Step 5: baseline probability of an individual
 # being approved for a mortgage.
 # 
 ##################################################
@@ -72,16 +119,6 @@ def update_totals(race: Races, row):
     else:
         total_not_approved[race] += 1
 
-def create_output_dataframe() -> pd.DataFrame:
-    list = []
-    for race in Races:
-        list.append([RACES_MAPPING[race],total_approved[race],total_not_approved[race],totals[race]])
-    list.append(['Total',sum(total_approved.values()),sum(total_not_approved.values()),sum(totals.values())])
-
-    df = pd.DataFrame.from_records(list)
-    df.columns = COLUMN_NAMES_OUTPUT
-    return df
-
 for idx, row in df_model_data.iterrows():
     race = row[COLUMN_NAMES[Columns.RACE]]    
     update_totals(race, row)
@@ -92,19 +129,28 @@ print(f'P(Approved|Overall) = {prob_of_approved_overall}\n')
 """
 ##################################################
 #
-# Questions 6: create table  
+# Step 6: create table  
 # 
 ##################################################
 """
 
+def create_output_dataframe() -> pd.DataFrame:
+    list = []
+    for race in Races:
+        list.append([RACES_MAPPING[race],total_approved[race],total_not_approved[race],totals[race]])
+    list.append(['Total',sum(total_approved.values()),sum(total_not_approved.values()),sum(totals.values())])
+
+    df = pd.DataFrame.from_records(list)
+    df.columns = COLUMN_NAMES_OUTPUT
+    return df
+
 output_df = create_output_dataframe()
 print(f'{output_df.to_string(index=False)}\n')
-
 
 """
 ##################################################
 #
-# Questions 7: calculate probabilities
+# Step 7: calculate probabilities
 # 
 ##################################################
 """
